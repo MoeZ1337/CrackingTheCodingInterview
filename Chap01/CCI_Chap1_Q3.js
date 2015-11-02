12df12a32709ebc902847667a37d74c96f5c5796
Muhammad <muhammad.n.e.zaman@gmail.com>
2015-11-02 14:18:25 -0800

    1.3 complete

--- Chap01/CCI_Chap1_Q3.js
+++ Chap01/CCI_Chap1_Q3.js
@@ -5,4 +5,50 @@
 	FOLLOW UP
 	Write the test cases for this method.*/
 
-	
+var str = 'this is a test!';
+
+doubleLoopRemoveDups = function(string) {
+	var charArr = string.split('');
+	for(var i = 0; i < charArr.length; i++) {
+		for(var j = i+1; j < charArr.length; j++) {
+			if(charArr[i] === charArr[j]) {
+				charArr.splice(j,1);
+				j--;
+			}
+		}
+	}
+	return charArr.join('');
+}
+
+// Misses space character
+sortRemoveDups = function(string) {
+	var charArr = string.split('').sort();
+	for(var i = 0; i < charArr.length - 1; i++) {
+		if(charArr[i] === charArr[i+1]) {
+			charArr.splice(i+1,1);
+			i--;
+		}
+	}
+	return charArr.join('');
+}
+
+tailRemoveDups = function(string) {
+	var charArr = string.split('');
+	var tail = 1;
+	var unique;
+	for(var i = 1; i < charArr.length; i++) {
+		unique = true;
+		for(var j = 0; j < tail; j++) {
+			console.log(charArr[i] + ' ?== ' + charArr[j]);
+			if(charArr[i] === charArr[j]) {
+				unique = false;
+				break;
+			}
+		}
+		if(unique) {
+			charArr[tail] = charArr[i];
+			tail++;
+		}
+	}
+	return charArr.join('').substr(0,tail);
+}
